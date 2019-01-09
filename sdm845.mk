3dc7525cd828d0d493b81ecdaa1ec0f90680a858
ethan-halsall <ethan.halsall@gmail.com>
2019-01-09 16:37:02 +0000

    Set lmkd options
    Signed-off-by: ethan-halsall <ethan.halsall@gmail.com>

--- sdm845.mk
+++ sdm845.mk
@@ -126,3 +126,14 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_BOOT_JARS += \
     WfdCommon
+
+# Set lmkd options
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    ro.lmk.low=1001 \
+    ro.lmk.medium=800 \
+    ro.lmk.critical=0 \
+    ro.lmk.critical_upgrade=false \
+    ro.lmk.upgrade_pressure=100 \
+    ro.lmk.downgrade_pressure=100 \
+    ro.lmk.kill_heaviest_task=true \
+    ro.lmk.kill_timeout_ms=100
